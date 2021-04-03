68b5cd400208e400edc429ee21a7e0b442bc116b
akhilmhdh <akhilmhdh@gmail.com>
2021-04-04 00:07:11 +0530

    fix: remove slash in hostname

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ function broadcast(data) {
 wss.on("connection", async (ws, req) => {
     const booths = await Booth.findAll({ order: [["createdAt", "ASC"]] });
     const highestTokenBooth = booths.reduce((prev, curr) => (prev?.a > curr.a ? prev : curr), {});
-    if (req.headers.host === "localhost:3000" || req.headers.host === "tokenboard.herokuapp.com/") {
+    if (req.headers.host === "localhost:3000" || req.headers.host === "tokenboard.herokuapp.com") {
         ws.send(`p-${JSON.stringify(booths)}`);
     } else {
         ws.send(
